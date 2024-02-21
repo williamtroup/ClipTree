fe6e199c8f260b8bfdcb55a2748f8056c22f5972
William Troup <williamtroup@gmail.com>
2024-02-21 19:13:40 +0000

    Added missing files.

--- ClipTree.Engine/Properties/AssemblyInfo.cs
+++ ClipTree.Engine/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ClipTree.Engine")]
-[assembly: AssemblyDescription("ClipTree is a small program to help you manage your clipboard history, enabling you to get back to something you thought you had lost.  A useful range of management tools is included to help you manage your lists as you go about your work.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bunoon")]
-[assembly: AssemblyProduct("ClipTree.Engine")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("be3a38da-0fc0-4d5b-8660-0e085c28cf83")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.4.2")]
-[assembly: AssemblyFileVersion("0.9.4.2")]
